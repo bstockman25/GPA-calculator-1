321374ff79c9763db21d26dc52452302531f47b5
Jason <jbpulis@yahoo.com>
2018-04-24 14:12:21 -0500

    moved controller message
    moved the MainController message to the main.php in views.

--- views/main.php
+++ views/main.php
@@ -1,4 +1,4 @@
 <!-- main -->
 
       <h1>Main</h1>
-      <h3>{{message}}</h3>
+      <h3>To create or add a new class to a semester simply slick add.To review or edit an existing semester please click edit If you are dine with the GPA monitor/forcaster just click logout (it is all pretty intuitive)</h3>
