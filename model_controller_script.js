015a575d457064d21975abffbc8451c3c3dde530
Eric <emitchell008@gmail.com>
2018-04-28 01:52:05 -0500

    reverted test changes on edit.php
    Have been testing http.get() in edit controller to pull json data from a php file, but have been unable to get the .get() to work properly. Current version works with test data hardcoded into edit controller, have commented out broken code to continue testing with later.

--- model_controller_script.js
+++ model_controller_script.js
@@ -44,14 +44,30 @@ app.controller('MainController', function($scope) {
   $scope.message = '';
 });
 
-app.controller('EditController', function($scope) {
-    // the GET request will need to go here
+app.controller('EditController', function($scope, $http) {
+    // test JSON data, this is the format that the data from the DB needs to be in
     $scope.semesters = [
-        {semester:'Fall',year:'2018',grade:4.0},
-        {semester:'Spring',year:'2018',grade:3.3},
-        {semester:'Summer',year:'2018',grade:3.7}
+        {'semester':'Fall','year':'2018','grade':4.0},
+        {'semester':'Spring','year':'2018','grade':3.3},
+        {'semester':'Summer','year':'2018','grade':3.7}
     ];
-  $scope.message = '';
+    // from: https://www.w3schools.com/angular/angular_sql.asp
+    // the get request should be able to get JSON data from the file and then load into the view, when it works. currently just displays the error message
+    
+//    $http.get("semesterData.php")
+//    .then(function(response) {
+//        //first function handles success
+//        $scope.semesters = response.data.records;
+//        $scope.message = 'success';
+//        $scope.statuscode = response.status;
+//        $scope.statustext = response.statusText;
+//    }, function(response) {
+//        //second function handles error
+//        $scope.message = 'error';
+//
+//    });
+
+  //$scope.message = '';
 });
 
 app.controller('NewController', function($scope) {
---
+++ semesterData.php
@@ -0,0 +1,7 @@
+{
+    'records':[
+        {'semester':'Fall','year':'2018','grade':4.0},
+        {'semester':'Spring','year':'2018','grade':3.3},
+        {'semester':'Summer','year':'2018','grade':3.7}
+    ]
+}
