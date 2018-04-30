93ecbf7255857a2220ca76a4cb7ac00e033e5681
Eric <emitchell008@gmail.com>
2018-04-29 23:50:33 -0500

    Updated model_controller_script.js "EditController" to read JSON data from php file
    EditController in model_controller_script.js has been updated to be able to read JSON data from semesterData.php file, and dynamically output this information to the screen. This gives us the format that data will need to be presented to the controller from the DB

--- semesterData.php
+++ semesterData.php
@@ -1,7 +1,29 @@
-{
-    'records':[
-        {'semester':'Fall','year':'2018','grade':4.0},
-        {'semester':'Spring','year':'2018','grade':3.3},
-        {'semester':'Summer','year':'2018','grade':3.7}
-    ]
-}
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+
+$outp = '{
+    "records" : [
+    {
+        "semester" : "Fall",
+        "year" : 2018,
+        "grade" : 4.0
+    },
+
+    {
+        "semester" : "Spring",
+        "year" : 2018,
+        "grade" : 3.3
+    },
+
+    {
+        "semester" : "Summer",
+        "year" : 2018,
+        "grade" : 3.7
+    }
+]
+}';
+
+echo($outp);
+?>
