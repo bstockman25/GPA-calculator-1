47c57bbb2422c9bb1d26ddf35096319fa4ffc9dc
Jason <jbpulis@yahoo.com>
2018-05-05 00:05:24 -0500

    grammer edit
    edit grammer and clean up a bit.

--- views/main.php
+++ views/main.php
@@ -1,3 +1,3 @@
 <!-- main -->
 <h1>Main</h1>
-<h3>To create or add a new class to a semester simply slick add.To review or edit an existing semester please click edit If you are dine with the GPA monitor/forcaster just click logout (it is all pretty intuitive)</h3>
+<h3>This GPA calculator can be used to track your GPA as well as forcast what you need in order to recieve a certain grade. To create or add a new class to a semester simply click add. To review or edit an existing semester please click edit (it is all pretty intuitive). To get started register and take controle of your academic planning today.</h3>
